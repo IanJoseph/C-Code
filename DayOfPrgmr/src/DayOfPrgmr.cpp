9fef07af1028253632102bb5943d1cf481e02624
Ian Joseph <smartgit@ianjoseph.com>
2017-07-12 14:51:26 -0700

    Flock of Birds - done

---
+++ DayOfPrgmr/src/DayOfPrgmr.cpp
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+char* solve(int year){
+    // Complete this function
+}
+
+int main() {
+    int year=0;
+    scanf("%d", &year);
+    int result_size;
+    char* result = solve(year);
+    printf("%s\n", result);
+    return 0;
+}

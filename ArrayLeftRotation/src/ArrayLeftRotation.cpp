bc1bbce17b3295645fcba26a1d2a166229b3445b
Ian Joseph <smartgit@ianjoseph.com>
2017-07-13 16:22:33 -0700

    Rotate an array of n elements by numLRotations

---
+++ ArrayLeftRotation/src/ArrayLeftRotation.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : ArrayLeftRotation.cpp
+// https://www.hackerrank.com/challenges/array-left-rotation
+//
+//============================================================================
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+	long int arrSize, numLRotations;
+	cin >> arrSize >> numLRotations;
+	vector <long int> arr;
+	arr.resize(arrSize);
+	for (long int i=0; i<arrSize; i++)
+		cin >> arr[i];
+	rotate(arr.begin(),arr.begin()+numLRotations,arr.end());
+	for (long int k=0; k<arrSize; k++)
+		printf("%li ",arr[k]);
+	printf ("\n");
+    return 0;
+}
